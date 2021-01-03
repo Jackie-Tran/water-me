c483d9d06dfe91965380d288b062af0541e2f631
Jackie <jtran2009a@gmail.com>
2021-01-03 17:54:43 -0500

    Created plant page

--- screens/plant/index.tsx
+++ screens/plant/index.tsx
@@ -1,6 +1,6 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RootStackParamList } from '../../NavigationTypes';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -26,7 +26,17 @@ const PlantScreen: React.FC<Props> = ({ navigation }) => {
             <MaterialIcons name="mode-edit" size={32} color="white" />
           </TouchableOpacity>
         </View>
-        <Text style={styles.headerText}>Plant Name</Text>
+        <Text style={styles.headerText}>Cassia</Text>
+      </View>
+      <View style={styles.content}>
+          <View style={styles.imageContainer}>
+              <Image style={styles.image} source={require('../../assets/dashboard/plant.jpg')}/>
+              <Text style={[styles.text, { fontSize: 36 }]}>Sunflower</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={[styles.text, { fontSize: 24 }]}>Water Time: 5:00pm</Text>
+            <Text style={[styles.text, { fontSize: 24 }]}>Every Monday</Text>
+          </View>
       </View>
     </SafeAreaView>
   );
@@ -59,6 +69,26 @@ const styles = StyleSheet.create({
     fontSize: 42,
     textAlign: 'center',
   },
+  content: {
+      paddingTop: '10%',
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'WorkSans_400Regular',
+    marginBottom: '5%',
+  },
+  image: {
+      width: 300,
+      height: 300,
+      borderRadius: 15,
+      marginBottom: '3%',
+  },
+  details: {
+    paddingHorizontal: '10%',
+  }
 });
 
 export default PlantScreen;
