8eb9155e70453f644a92cda61375065418b0604d
Jackie <jtran2009a@gmail.com>
2021-01-03 01:04:58 -0500

    Created login screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RootStackParamList } from './NavigationTypes';
+import LoginScreen from './screens/login';
+import {
+  Merriweather_300Light,
+  Merriweather_300Light_Italic,
+  Merriweather_400Regular,
+  Merriweather_400Regular_Italic,
+  Merriweather_700Bold,
+  Merriweather_700Bold_Italic,
+  Merriweather_900Black,
+  Merriweather_900Black_Italic,
+  useFonts,
+} from '@expo-google-fonts/merriweather';
+import { 
+    WorkSans_100Thin,
+    WorkSans_200ExtraLight,
+    WorkSans_300Light,
+    WorkSans_400Regular,
+    WorkSans_500Medium,
+    WorkSans_600SemiBold,
+    WorkSans_700Bold,
+    WorkSans_800ExtraBold,
+    WorkSans_900Black,
+    WorkSans_100Thin_Italic,
+    WorkSans_200ExtraLight_Italic,
+    WorkSans_300Light_Italic,
+    WorkSans_400Regular_Italic,
+    WorkSans_500Medium_Italic,
+    WorkSans_600SemiBold_Italic,
+    WorkSans_700Bold_Italic,
+    WorkSans_800ExtraBold_Italic,
+    WorkSans_900Black_Italic 
+  } from '@expo-google-fonts/work-sans'
+  import AppLoading from 'expo-app-loading';
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Merriweather_300Light,
+    Merriweather_300Light_Italic,
+    Merriweather_400Regular,
+    Merriweather_400Regular_Italic,
+    Merriweather_700Bold,
+    Merriweather_700Bold_Italic,
+    Merriweather_900Black,
+    Merriweather_900Black_Italic,
+    WorkSans_100Thin,
+    WorkSans_200ExtraLight,
+    WorkSans_300Light,
+    WorkSans_400Regular,
+    WorkSans_500Medium,
+    WorkSans_600SemiBold,
+    WorkSans_700Bold,
+    WorkSans_800ExtraBold,
+    WorkSans_900Black,
+    WorkSans_100Thin_Italic,
+    WorkSans_200ExtraLight_Italic,
+    WorkSans_300Light_Italic,
+    WorkSans_400Regular_Italic,
+    WorkSans_500Medium_Italic,
+    WorkSans_600SemiBold_Italic,
+    WorkSans_700Bold_Italic,
+    WorkSans_800ExtraBold_Italic,
+    WorkSans_900Black_Italic 
+  });
+
+  if (!fontsLoaded) {
+      return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/login/index.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from 'react-native';
+import {
+  SafeAreaView,
+  withSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const LoginScreen: React.FC = () => {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require('../../assets/login/background.png')}
+    >
+      <View style={styles.overlay}>
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.title}>Water.me</Text>
+          <View style={styles.loginContainer}>
+            <View style={styles.textInput}>
+              <MaterialIcons name="email" size={28} color="#52B788" />
+              <TextInput style={styles.input} placeholder="email" />
+            </View>
+            <View style={styles.textInput}>
+              <MaterialIcons name="lock" size={28} color="#52B788" />
+              <TextInput style={styles.input} placeholder="password" />
+            </View>
+            <TouchableOpacity>
+              <Text style={[styles.text, { color: '#0066FF', alignSelf: 'flex-end' }]}>Forgot your password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Log In</Text>
+            </TouchableOpacity>
+            <View style={{ flexDirection: 'row', }}>
+              <Text style={styles.text}>Don't have an account? </Text>
+              <TouchableOpacity>
+                <Text style={[styles.text, { color: '#0066FF' }]}>Create an account</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </SafeAreaView>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+  overlay: {
+    backgroundColor: 'rgba(167, 167, 167, 0.7)',
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'Merriweather_400Regular',
+    fontSize: 72,
+    color: 'white',
+  },
+  loginContainer: {},
+  textInput: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: 300,
+    height: 50,
+    paddingLeft: '3%',
+    marginBottom: '5%',
+  },
+  input: {
+    marginLeft: '3%',
+    fontFamily: 'WorkSans_400Regular',
+    fontSize: 24,
+    flex: 1,
+  },
+  text: {
+    fontFamily: 'WorkSans_400Regular',
+    fontSize: 14,
+    color: 'white',
+  },
+  button: {
+    backgroundColor: '#52B788',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    height: 55,
+    marginVertical: '5%',
+  },
+  buttonText: {
+    fontFamily: 'WorkSans_400Regular',
+    fontSize: 36,
+    color: 'white',
+  },
+});
+
+export default LoginScreen;
