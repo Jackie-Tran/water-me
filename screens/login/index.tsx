7566584bb4f7c294a34e668a41be0e55249269e0
Jackie <jtran2009a@gmail.com>
2021-01-03 01:07:47 -0500

    Created dismiss keyboard component for login screen

--- screens/login/index.tsx
+++ screens/login/index.tsx
@@ -7,11 +7,9 @@ import {
   View,
   TextInput,
 } from 'react-native';
-import {
-  SafeAreaView,
-  withSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { MaterialIcons } from '@expo/vector-icons';
+import DismissKeyboard from '../../components/dismiss-keyboard';
 
 const LoginScreen: React.FC = () => {
   return (
@@ -19,33 +17,44 @@ const LoginScreen: React.FC = () => {
       style={styles.background}
       source={require('../../assets/login/background.png')}
     >
-      <View style={styles.overlay}>
-        <SafeAreaView style={styles.container}>
-          <Text style={styles.title}>Water.me</Text>
-          <View style={styles.loginContainer}>
-            <View style={styles.textInput}>
-              <MaterialIcons name="email" size={28} color="#52B788" />
-              <TextInput style={styles.input} placeholder="email" />
-            </View>
-            <View style={styles.textInput}>
-              <MaterialIcons name="lock" size={28} color="#52B788" />
-              <TextInput style={styles.input} placeholder="password" />
-            </View>
-            <TouchableOpacity>
-              <Text style={[styles.text, { color: '#0066FF', alignSelf: 'flex-end' }]}>Forgot your password?</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button}>
-              <Text style={styles.buttonText}>Log In</Text>
-            </TouchableOpacity>
-            <View style={{ flexDirection: 'row', }}>
-              <Text style={styles.text}>Don't have an account? </Text>
+      <DismissKeyboard>
+        <View style={styles.overlay}>
+          <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Water.me</Text>
+            <View style={styles.loginContainer}>
+              <View style={styles.textInput}>
+                <MaterialIcons name="email" size={28} color="#52B788" />
+                <TextInput style={styles.input} placeholder="email" />
+              </View>
+              <View style={styles.textInput}>
+                <MaterialIcons name="lock" size={28} color="#52B788" />
+                <TextInput style={styles.input} placeholder="password" />
+              </View>
               <TouchableOpacity>
-                <Text style={[styles.text, { color: '#0066FF' }]}>Create an account</Text>
+                <Text
+                  style={[
+                    styles.text,
+                    { color: '#0066FF', alignSelf: 'flex-end' },
+                  ]}
+                >
+                  Forgot your password?
+                </Text>
               </TouchableOpacity>
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Log In</Text>
+              </TouchableOpacity>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.text}>Don't have an account? </Text>
+                <TouchableOpacity>
+                  <Text style={[styles.text, { color: '#0066FF' }]}>
+                    Create an account
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
-          </View>
-        </SafeAreaView>
-      </View>
+          </SafeAreaView>
+        </View>
+      </DismissKeyboard>
     </ImageBackground>
   );
 };
