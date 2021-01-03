6e5bae6e43a5e15b91069c67be68df80ec212993
Jackie <jtran2009a@gmail.com>
2021-01-03 17:45:29 -0500

    Added header to plant screen

--- screens/plant/index.tsx
+++ screens/plant/index.tsx
@@ -1,12 +1,64 @@
+import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { RootStackParamList } from '../../NavigationTypes';
+import { MaterialIcons } from '@expo/vector-icons';
 
-const PlantScreen: React.FC = () => {
-    return(
-        <View>
-            <Text>Plant Screen</Text>
+type NavProp = StackNavigationProp<RootStackParamList, 'Your Plants'>;
+
+type Props = {
+  navigation: NavProp;
+};
+const PlantScreen: React.FC<Props> = ({ navigation }) => {
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <SafeAreaView edges={['right', 'left', 'bottom']}>
+      <View style={styles.header}>
+        <View style={styles.navbar}>
+          <TouchableOpacity onPress={handleBackPress}>
+            <MaterialIcons name="arrow-back" size={32} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <MaterialIcons name="mode-edit" size={32} color="white" />
+          </TouchableOpacity>
         </View>
-    );
-}
+        <Text style={styles.headerText}>Plant Name</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#40916C',
+    height: 150,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    paddingVertical: '10%',
+    paddingHorizontal: '3%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+    elevation: 5,
+  },
+  navbar: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+  },
+  headerText: {
+    fontFamily: 'Merriweather_400Regular',
+    color: 'white',
+    fontSize: 42,
+    textAlign: 'center',
+  },
+});
 
 export default PlantScreen;
