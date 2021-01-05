aa62aa1ef9d9f8611a2d5ec9227a81e4435a75cf
Jackie <jtran2009a@gmail.com>
2021-01-05 14:45:49 -0500

    Added route to get a user

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,5 @@
 import Router from 'express-promise-router';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, query } from 'express';
 import db from '../db';
 
 const router = Router();
@@ -22,18 +22,23 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
       [uid, firstName, lastName, email]
     )
     .then((queryResult) => {
-        res.json({ message: 'Successfully added a new user' });
+      res.json({ message: 'Successfully added a new user' });
     })
     .catch((err) => {
-        res.status(412);
-        res.json(err);
+      res.status(412);
+      res.json(err);
     });
 });
 
 // Read user
-router.get(
-  '/:uid',
-  async (req: Request, res: Response, next: NextFunction) => {}
-);
+router.get('/:uid', async (req: Request, res: Response, next: NextFunction) => {
+  const { uid } = req.params;
+  const { rows } = await db.query('SELECT * FROM public.users WHERE uid=$1', [uid]);
+  if (rows[0]) {
+      res.json(rows[0]);
+  }
+  res.status(404);
+  res.json({ message: 'Unable find user with that uid' });
+});
 
 module.exports = router;
