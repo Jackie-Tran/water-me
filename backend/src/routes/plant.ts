2eea41ae22a4edd82de14a5d50a01f380fe078f0
Jackie <jtran2009a@gmail.com>
2021-01-05 16:28:25 -0500

    Added route to delete plants

--- backend/src/routes/plant.ts
+++ backend/src/routes/plant.ts
@@ -4,10 +4,6 @@ import db from '../db';
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  res.json('Plants Route');
-});
-
 // Create plant
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
   // TODO: Check if uid exists in firebase or postgres
@@ -40,7 +36,7 @@ router.get('/:uid', async (req: Request, res: Response, next: NextFunction) => {
 router.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
   const { name, type, waterTime, repeat } = req.body;
-  console.log(repeat);
+  
   let query = 'UPDATE public.plants SET ';
   // Build query
   if (name) {
@@ -57,9 +53,20 @@ router.put('/:id', async (req: Request, res: Response, next: NextFunction) => {
   }
   query = query.substr(0, query.lastIndexOf(','));
   query += " WHERE id='" + id + "'";
-  console.log(query);
+  
   const { rowCount } = await db.query(query, []);
   res.json({ message: 'Updated ' + rowCount + ' plant' });
+});
+
+// Delete plant
+router.delete('/:id', async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const {rowCount} = await db.query('DELETE FROM public.plants WHERE id=$1', [id]);
+    if (rowCount > 0) {
+        res.json({ message: 'Successfully deleted the plant' });
+    }
+    res.status(404);
+    res.json({ message: 'Unable to delete the plant with that id' });
 });
 
 module.exports = router;
