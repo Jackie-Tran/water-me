76b4c3446e4394a7c24131611e7ff1562c0ab344
Jackie <jtran2009a@gmail.com>
2021-01-05 15:22:46 -0500

    Added route for deleting a user

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -33,9 +33,11 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
 // Read user
 router.get('/:uid', async (req: Request, res: Response, next: NextFunction) => {
   const { uid } = req.params;
-  const { rows } = await db.query('SELECT * FROM public.users WHERE uid=$1', [uid]);
+  const { rows } = await db.query('SELECT * FROM public.users WHERE uid=$1', [
+    uid,
+  ]);
   if (rows[0]) {
-      res.json(rows[0]);
+    res.json(rows[0]);
   }
   res.status(404);
   res.json({ message: 'Unable find user with that uid' });
@@ -43,25 +45,36 @@ router.get('/:uid', async (req: Request, res: Response, next: NextFunction) => {
 
 // Update user
 router.put('/:uid', async (req: Request, res: Response, next: NextFunction) => {
-    const { uid } = req.params;
-    const { firstName, lastName, email } = req.body;
-    let query = 'UPDATE public.users SET ';
-    // Build query
-    if (firstName) {
-        query += "first_name='" + firstName + "', ";
-    }
-    if (lastName) {
-        query += "last_name='" + lastName + "', ";
-    }
-    if (email) {
-        query += "email='" + email + "',";
-    }
-    query = query.substr(0, query.lastIndexOf(','));
-    query += " WHERE uid='" + uid + "'";
-    console.log(query);
-    const { rowCount } = await db.query(query, []);
-
-    res.json({ message: 'Updated ' + rowCount + ' user(s)' });
+  const { uid } = req.params;
+  const { firstName, lastName, email } = req.body;
+  let query = 'UPDATE public.users SET ';
+  // Build query
+  if (firstName) {
+    query += "first_name='" + firstName + "', ";
+  }
+  if (lastName) {
+    query += "last_name='" + lastName + "', ";
+  }
+  if (email) {
+    query += "email='" + email + "',";
+  }
+  query = query.substr(0, query.lastIndexOf(','));
+  query += " WHERE uid='" + uid + "'";
+  
+  const { rowCount } = await db.query(query, []);
+  res.json({ message: 'Updated ' + rowCount + ' user(s)' });
 });
+
+// Delete user
+router.delete('/:uid', async (req: Request, res: Response, next: NextFunction) => {
+    const { uid } = req.params;
+    const {rowCount} = await db.query('DELETE FROM public.users WHERE uid=$1', [uid]);
+    if (rowCount > 0) {
+        res.json({ message: 'Successfully deleted the user' });
+    }
+    res.status(404);
+    res.json({ message: 'Unable to delete the user with that uid' });
+  }
+);
 
 module.exports = router;
