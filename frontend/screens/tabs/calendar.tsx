9c22e7022b5b6880194052ff4279b0fd89957c5e
Jackie <jtran2009a@gmail.com>
2021-01-08 00:18:15 -0500

    Added placeholder content for calendar tab

--- frontend/screens/tabs/calendar.tsx
+++ frontend/screens/tabs/calendar.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const CalendarScreen: React.FC = () => {
     return (
-        <View>
-            <Text>
-                Calendar
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.text}>
+                Calendar feature is coming soon!
             </Text>
-        </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    text: {
+        fontFamily: 'Merriweather_400Regular',
+        color: 'black',
+        fontSize: 36,
+        textAlign: 'center',
+    }
+});
 
 export default CalendarScreen;
